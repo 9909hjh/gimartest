216edf6abb3677dbd6048e53309782849f87c8a2
9909hjh <9909hjh@gmail.com>
2022-06-13 13:20:56 +0900

    연습함
    연습함

--- HoseoSDL/src/Vehicle.cpp
+++ HoseoSDL/src/Vehicle.cpp
@@ -1,40 +1,15 @@
 #include "Vehicle.h"
 #include <random>
 
-Vehicle::Vehicle(int x, int y) : maxSpeed(4), r(16), maxForce(0.25f), 
-distance(0), dec(1.f), speed(0)
+Vehicle::Vehicle()
 {
-	m_pos = new Vector2D(x, y);
-	m_vel = new Vector2D(0, 0);
-	m_acc = new Vector2D(0, 0);
-	m_force = new Vector2D(0, 0);
 
-	rdi1 = new Vector2D(0, 0);
-	rdi2 = new Vector2D(0, 0);
-	rdi3 = new Vector2D(0, 0);
 }
 
 void Vehicle::update()
 {
-	*m_vel += *m_acc;
-	m_vel->limit(maxSpeed);
-	*m_pos += *m_vel;
-	*m_acc *= 0;
 
-	radian = atan2(m_vel->getY(), m_vel->getX());
-	*rdi1 = Rradian(-r, -r / 2, radian);
-	*rdi2 = Rradian(-r, r / 2, radian);
-	*rdi3 = Rradian(r, 0, radian);
-}
 
-void Vehicle::applyForce(Vector2D* force)
-{
-	*m_acc += *force;
-}
-
-Vector2D Vehicle::arrive(Vector2D* target)
-{
-	return seek(target);
 }
 
 Vector2D Vehicle::seek(Vector2D* target)
@@ -55,16 +30,6 @@ Vector2D Vehicle::seek(Vector2D* target)
 	}
 
 	return Vector2D(0, 0);
-}
-
-Vector2D Vehicle::Rradian(float x, float y, float radian)
-{
-	Vector2D rota(0, 0);
-
-	rota.setX(cos(radian) * x - sin(radian) * y);
-	rota.setY(sin(radian) * x + cos(radian) * y);
-
-	return rota;
 }
 
 void Vehicle::draw(SDL_Renderer* renderer)
